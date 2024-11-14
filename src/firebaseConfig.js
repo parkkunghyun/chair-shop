7e3c82d4d411471a7704b933e52e56b3062492b7
parkkunghyun <rudgus4620@gmail.com>
2024-11-14 16:13:51 +0900

    asdfa

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
+import { GoogleAuthProvider } from "firebase/auth";
 
 // Firebase 초기화
 const firebaseConfig = {
@@ -17,3 +18,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
+
