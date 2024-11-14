9411c53803b076d1bef24c4908b9bbb527cf4519
parkkunghyun <rudgus4620@gmail.com>
2024-11-14 16:35:53 +0900

    asdf

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-import { GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut } from "firebase/auth";
 
 // Firebase 초기화
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  projectId: "calendar-app-dedd2",
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
@@ -19,3 +19,9 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const signInWithGoogle = () => signInWithPopup(auth, googleProvider);
+export const signInWithEmail = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const signUpWithEmail = (email, password) => createUserWithEmailAndPassword(auth, email, password);
+export const logout = () => signOut(auth);
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 
 const LoginPage = () => {
+  
   return (
-    <div>LoginPage</div>
+    <div className='flex flex-col items-center'>
+          <h1 class="bg-gradient-to-r mb-8 text-5xl font-extrabold from-blue-600 via-green-500 to-indigo-400 inline-block text-transparent bg-clip-text">
+              로그인
+          </h1>
+      <div>
+        
+      </div>
+    </div>
   )
 }
 
